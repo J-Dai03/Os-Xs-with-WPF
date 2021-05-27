c4a1bc438708d6274bb35d31e0f41b5e5ea30cb3
James Dai <K038246@eltham-college.org.uk>
2021-05-27 15:35:40 +0100

    Imported and adapted Board class from 0sandXs project from March

---
+++ WpfApp1/Board.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Board
+    {
+        //Data is stroed in a 3X3 2D array of integers
+        //Empty cells are represented with 0
+        //X cells are represented with 1
+        //O cells are represented with 2
+
+        private int[,] pattern;
+
+        public Board()
+        {
+            // 3x3 board
+            pattern = new int[3,3];
+            clearBoard();
+        }
+        public void clearBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    pattern[i,j] = 0;
+                }
+            }
+        }
+
+        public bool addCounter(int playerSymbol, int xPos, int yPos)
+        {
+            bool result = false;
+            if (pattern[xPos, yPos] == 0)
+            {
+                pattern[xPos, yPos] = playerSymbol;
+                result = true;
+            }
+            else if ((pattern[xPos, yPos] > 2)|| (pattern[xPos, yPos] < 0))
+            {
+                //Value out of bounds
+                Console.WriteLine("Rep value out of bounds");
+                result = false;
+            }
+            return result;
+        }
+        public int checkwin()
+        {
+            // 0 - Draw
+            // 1 - Player 1 wins
+            // 2 - Player 2 wins
+            // 3 - no Win -> continue
+
+            //Variables
+            int result = 3;
+            bool FullBoard = true;
+
+            //check for draw
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (pattern[i, j] == 0)
+                    {
+                        FullBoard = false;
+                    }
+                }
+            }
+            if (FullBoard == true)
+            {
+                result = 0;
+            }
+            //if no draw has been detected yet
+            if (result != 0)
+            {
+                //check for win
+                for (int i = 0; i < 3; i++)
+                {
+                    //Row check
+                    if (pattern[i, 0] == pattern[i, 1] &&
+                        pattern[i, 0] == pattern[i, 2])
+                    {
+                        result = pattern[i, 0];
+                    }
+                    //Column check
+                    else if (pattern[0, i] == pattern[1, i] &&
+                        pattern[0, i] == pattern[2, i])
+                    {
+                        result = pattern[0, i];
+                    }
+                }
+
+                //diagonal checks
+                if (pattern[0, 0] == pattern[1, 1] &&
+                    pattern[0, 0] == pattern[2, 2])
+                {
+                    result = pattern[0, 0];
+                }
+                else if (pattern[2, 0] == pattern[1, 1] &&
+                    pattern[2, 0] == pattern[0, 2])
+                {
+                    result = pattern[2, 0];
+                }
+
+            }
+            return result;
+        }
+        public int GetPos(int xPos, int yPos)
+        {
+            return (pattern[xPos, yPos]);
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -26,9 +26,53 @@ namespace WpfApp1
         }
 
 
-        private void Board_Button(object sender, RoutedEventArgs e)
+        private void Button_Click_0(object sender, RoutedEventArgs e)
         {
-            Pos03.Content = "X";
+            Display.Text = "0";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "1";
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "2";
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "3";
+        }
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "4";
+        }
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "5";
+        }
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "6";
+        }
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "7";
+        }
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "8";
+        }
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "Reset";
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "Start";
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "Save";
         }
     }
 }
